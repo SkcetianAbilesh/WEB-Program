aa5e048ca0325c50cad0e7cc19b08341def14b11
SkcetianAbilesh <124855926+SkcetianAbilesh@users.noreply.github.com>
2024-03-04 09:31:13 +0530

    Delete DAY11 directory

--- DAY11/d11ex2/src/main/java/com/example/d11ex2/repo/StudentIdRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.d11ex2.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.d11ex2.model.StudentId;
-
-public interface StudentIdRepo extends JpaRepository<StudentId,Integer> {
-    
-}
--- DAY11/d11ex2/src/main/java/com/example/d11ex2/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.d11ex2.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.d11ex2.model.Student;
-
-public interface StudentRepo  extends JpaRepository<Student,Integer>{
-    
-}
